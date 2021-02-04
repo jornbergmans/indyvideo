2b004233b653b8df375e8a52c4a033bedb5ff416
Jorn <14345070+jornbergmans@users.noreply.github.com>
2021-02-04 18:43:45 +0100

    Update auto_ftp.sh

--- auto_ftp/auto_ftp.sh
+++ auto_ftp/auto_ftp.sh
@@ -5,19 +5,35 @@ OUTFOLDER="/Volumes/CONTENTLAB/IFFR2021/STUDIOS/STUDIO_UIT"
 DATE="$(date "+%m_%d")"
 TIME="$(date "+%H%M")"
 
+check_lftp() {
+
+if [ -z "${LFTP}" ]; then
+  curl -JL "https://lftp.tech/ftp/lftp-4.9.2.tar.gz" --output "$PWD/lftp-4.9.2.tar.gz"
+  tar xf "$PWD/lftp-4.9.2.tar.gz"
+  cd "./lftp-4.9.2" || echo "Could not change directory to build lftp." && exit
+  ./configure
+  make
+  make install
+  LFTP="$PWD/lftp-4.9.2/lftp"
+  cd - || exit
+#  cd "$(dirname "${0}")" || exit
+fi
+
+            }
+
+
 get_files() {
+
   LOGIN="root:root@${IP}"
-
   echo "Connecting to ${IP}."
-
-  RDIRS="$(/usr/local/bin/lftp -e 'find -d 2; quit;' "${IP}")"
+  RDIRS="$("${LFTP}" -e 'find -d 2; quit;' "${IP}")"
 
   for RDIR in ${RDIRS}; do
     if [ "${RDIR}" = "./1/" ] || [ "${RDIR}" = "./2/" ]; then
       echo "Sync job for $DATE at $TIME"
       echo "Scanning ${RDIR}."
-      MCCTEST="$(/usr/local/bin/lftp -e 'glob echo "*mcc"; quit;' "${LOGIN}"/"${RDIR}")"
-      MOVTEST="$(/usr/local/bin/lftp -e 'glob echo "*mov"; quit;' "${LOGIN}"/"${RDIR}")"
+      MCCTEST="$("${LFTP}" -e 'glob echo "*mcc"; quit;' "${LOGIN}"/"${RDIR}")"
+      MOVTEST="$("${LFTP}" -e 'glob echo "*mov"; quit;' "${LOGIN}"/"${RDIR}")"
 
 #      echo "MCC test == ${MCCTEST}"
 #      echo "Mov test == ${MOVTEST}"
@@ -28,11 +44,11 @@ get_files() {
         echo "No MCC file found, checking for mov file."
         echo "Mov file found, syncing directory to ${OUTLOC}."
             mkdir -p "${OUTLOC}"
-#        /usr/local/bin/lftp -e "cls -1l; quit;" "${LOGIN}"/"${RDIR}"
+#        "${LFTP}" -e "cls -1l; quit;" "${LOGIN}"/"${RDIR}"
         if [ -d "${OUTLOC}" ]; then
           cd "${OUTLOC}" || exit
-          /usr/local/bin/lftp -e 'mirror -c -f *.mov ; quit;' "${LOGIN}"/"${RDIR}"
-#          /usr/local/bin/lftp -e 'mirror -c -f *.mov --Remove-source-files ; quit;' "${LOGIN}"/"${RDIR}"
+#          "${LFTP}" -e 'mirror -c -f *.mov ; quit;' "${LOGIN}"/"${RDIR}"
+          "${LFTP}" -e 'mirror -c -f *.mov --Remove-source-files ; quit;' "${LOGIN}"/"${RDIR}"
           echo "Transfer complete. Sync ok."
           cd - || exit
         elif [ ! -d "${OUTLOC}" ]; then
@@ -46,28 +62,56 @@ get_files() {
   done
 }
 
-for STUDIO in 1 2 3 4; do
 
-#  if [ "${STUDIO}" = "1" ]; then
-#    STUDIO_NAME="INBEL_1"
-#    IPRANGE="121 122 123"
-  if [ "${STUDIO}" = "2" ]; then
-    STUDIO_NAME="INBEL_2"
-    IPRANGE="124 141"
-  else
-    IPRANGE=""
-  fi
+set_studios() {
+
+STUDIOLIST="$(dirname "$0")/STUDIOLIST.txt"
 
 
+  if ! -f 
+
+  while read -r line; do
+#    if [ -n "$line" ] && [ "$line" =~ ^([0-9]{3}\.){3}[0-9]{3}& ] ; # NOT POSIX COMPLIANT BUT PRETTIER IN BASH
+    if [ -n "${line}" ] && ! grep -Eq "^([0-9]{3}\.){3}[0-9]{3}&" < "${line}" ; then
+      STUDIOS="$STUDIOS $line"
+    fi
+  done < "${STUDIOLIST}"
+}
+
+set_ip() {
+
+  while read -r line; do
+    if [ "$STUDIO" = "$line" ]; then
+      while read -r line; do
+        if [ -n "${line}" ] && [ "${line}" = "$(echo "$line" | grep -Eq "^([0-9]{3}\.){3}[0-9]{3}&")" ]; then
+          IPRANGE="$IPRANGE $line"
+        fi
+      done
+    fi
+  done < "$(dirname "$0")/STUDIOLIST.txt"
+
+}
+
+
+### IF NAME IS MAIN
+
+main() {
+
+check_lftp
+
+set_studios
+
+for STUDIO in $STUDIOS; do
+  set_ip
   if [ -n "${IPRANGE}" ]; then
-    for IPVAR in ${IPRANGE}; do
-      OUTLOC="${OUTFOLDER}/${STUDIO_NAME}/${DATE}/SYNC_${TIME}/${IPVAR}"
-#      echo "${IPVAR}"
-      IP="192.168.110.${IPVAR}"
-#      echo "${IP}"
+    for IP in ${IPRANGE}; do
+      IPSTUB="${IP:-3}"
+      OUTLOC="${OUTFOLDER}/${STUDIO}/${DATE}/SYNC_${TIME}/${IPSTUB}"
       get_files
     done
   fi
-
 done
 
+}
+
+main
