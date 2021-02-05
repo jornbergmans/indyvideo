e61797a532c8ad9e733dd1423758af38db833712
Jorn <14345070+jornbergmans@users.noreply.github.com>
2021-02-05 15:25:03 +0100

    Update auto_ftp.sh
    DEZE VERSIE IS STUK, NIET GEBRUIKEN!

--- auto_ftp/auto_ftp.sh
+++ auto_ftp/auto_ftp.sh
@@ -4,22 +4,23 @@ LFTP="$(command -v lftp)"
 OUTFOLDER="/Volumes/CONTENTLAB/IFFR2021/STUDIOS/STUDIO_UIT"
 DATE="$(date "+%m_%d")"
 TIME="$(date "+%H%M")"
+STUDIOLIST="$(dirname "$0")/STUDIOLIST.txt"
 
 check_lftp() {
 
-if [ -z "${LFTP}" ]; then
-  curl -JL "https://lftp.tech/ftp/lftp-4.9.2.tar.gz" --output "$PWD/lftp-4.9.2.tar.gz"
-  tar xf "$PWD/lftp-4.9.2.tar.gz"
-  cd "./lftp-4.9.2" || echo "Could not change directory to build lftp." && exit
-  ./configure
-  make
-  make install
-  LFTP="$PWD/lftp-4.9.2/lftp"
-  cd - || exit
-#  cd "$(dirname "${0}")" || exit
-fi
+  if [ -z "${LFTP}" ]; then
+    curl -JL "https://lftp.tech/ftp/lftp-4.9.2.tar.gz" --output "$PWD/lftp-4.9.2.tar.gz"
+    tar xf "$PWD/lftp-4.9.2.tar.gz"
+    cd "./lftp-4.9.2" || echo "Could not change directory to build lftp." && exit
+    ./configure
+    make
+    make install
+    LFTP="$PWD/lftp-4.9.2/lftp"
+#    cd - || exit
+    cd "$(dirname "${0}")" || exit
+  fi
 
-            }
+}
 
 
 get_files() {
@@ -63,12 +64,66 @@ get_files() {
 }
 
 
+set_ip() {
+
+  echo "Please add IP address of recorder for ${STUDIO_INPUT}."
+  read -r STUDIO_IP
+  echo "${STUDIO_IP}" >> "${STUDIOLIST}"
+  echo "Do you want to input another source location?"
+  read -r answer
+  yn="$(echo "${answer}" | /usr/bin/env tr '[:upper:]' '[:lower:]')"
+  case ${yn} in
+    yes )
+      echo "" >> "${STUDIO_IP}"
+      set_ip
+      ;;
+    no)
+      pass
+      ;;
+    *)
+      echo "Please input yes or no."
+      ;;
+  esac
+
+}
+
+
 set_studios() {
 
-STUDIOLIST="$(dirname "$0")/STUDIOLIST.txt"
+  echo "Please input studio location name."
+  read -r STUDIO_INPUT
+  echo "Adding ${STUDIO_INPUT} to list of sources."
+  echo "${STUDIO_INPUT}" >> "${STUDIOLIST}"
+
+  set_ip
+
+  echo "Do you want to input another source location?"
+  read -r answer
+  yn="$(echo "${answer}" | /usr/bin/env tr '[:upper:]' '[:lower:]')"
+  case ${yn} in
+    yes )
+      echo "" >> "${STUDIOLIST}"
+      set_studios
+      ;;
+    no )
+      echo "Current list of sources is ${STUDIOLIST}."
+#      echo "Call this script with the -a flag to add more source lcoations."
+      get_studios
+      ;;
+    * )
+      echo "Please input yes or no."
+      ;;
+  esac
+
+}
 
 
-  if ! -f 
+get_studios() {
+
+  if [ ! -f "${STUDIOLIST}" ] ; then
+    echo "No sources specified."
+    set_studios
+  fi
 
   while read -r line; do
 #    if [ -n "$line" ] && [ "$line" =~ ^([0-9]{3}\.){3}[0-9]{3}& ] ; # NOT POSIX COMPLIANT BUT PRETTIER IN BASH
@@ -99,7 +154,7 @@ main() {
 
 check_lftp
 
-set_studios
+get_studios
 
 for STUDIO in $STUDIOS; do
   set_ip
